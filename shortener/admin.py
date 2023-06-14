cab6bbfa76b7b4df11f4aa40d164702443bd88b3
toxuh <zakharov.toxa@gmail.com>
2023-06-14 22:37:30 +0200

    styles and more

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ShortenedLink
+
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'link', 'shortened_link', 'creation_date', 'user_ip', 'user_agent')
+    list_filter = ('creation_date',)
+
+
+admin.site.register(ShortenedLink, LinkAdmin)
--- shortener/urls.py
+++ shortener/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'shortener'
+
 urlpatterns = [
     path('', views.create_short_url, name='create'),
     path('<str:short_url>', views.redirect_to_original, name='redirect'),
